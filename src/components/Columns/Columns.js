be77674c03e27d5eddb2acf0f9e533d7361d34d3
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-10-15 14:23:03 +0200

    Creating withAuth hoc

--- src/components/Columns/Columns.js
+++ src/components/Columns/Columns.js
@@ -2,8 +2,10 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './Columns.module.scss';
 import withCollapse from '../../hoc/withCollapse';
+import withAuth from '../../hoc/withAuth';
+import { compose } from 'recompose';
 
-const Columns = ({ toggle, isCollapsed }) => {
+const Columns = ({ toggle, isCollapsed, isAuthorised, toggleAuth }) => {
   const rowClass = cx('columns', {
     [styles.isCollapsed]: isCollapsed
   });
@@ -11,26 +13,37 @@ const Columns = ({ toggle, isCollapsed }) => {
   return (
     <div className='columns'>
       <div className='column'>
+        <p>Authorised: {isAuthorised.toString()}</p>
         <button className='button is-dark is-large' onClick={toggle}>
           Hellow
         </button>
-        <div className={rowClass}>
-          <div className='column'>
-            <div className='notification is-primary'>First column</div>
+        <button className='button is-large is-warning' onClick={toggleAuth}>
+          {isAuthorised ? 'logout' : 'login'}
+        </button>
+        {isAuthorised ? (
+          <div className={rowClass}>
+            <div className='column'>
+              <div className='notification is-primary'>First column</div>
+            </div>
+            <div className='column'>
+              <div className='notification is-primary'>Second column</div>
+            </div>
+            <div className='column'>
+              <div className='notification is-primary'>Third column</div>
+            </div>
+            <div className='column'>
+              <div className='notification is-primary'>Fourth column</div>
+            </div>
           </div>
-          <div className='column'>
-            <div className='notification is-primary'>Second column</div>
-          </div>
-          <div className='column'>
-            <div className='notification is-primary'>Third column</div>
-          </div>
-          <div className='column'>
-            <div className='notification is-primary'>Fourth column</div>
-          </div>
-        </div>
+        ) : (
+          <h2 className='title '>You must sign in</h2>
+        )}
       </div>
     </div>
   );
 };
 
-export default withCollapse(Columns);
+export default compose(
+  withAuth,
+  withCollapse
+)(Columns);

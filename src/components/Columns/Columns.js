9fb4b1d1eeff241639c8f2e3a81f229f6c4fb5ba
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-10-15 16:15:55 +0200

    Creating dropdown list with Downshift

--- src/components/Columns/Columns.js
+++ src/components/Columns/Columns.js
@@ -1,42 +1,64 @@
 import React from 'react';
 import cx from 'classnames';
 import styles from './Columns.module.scss';
+import Authorisation from '../../providers/Authorisation';
+import Collapse from '../../providers/Collapse';
 
 const Columns = ({ toggle, isCollapsed, isAuthorised, toggleAuth }) => {
-  const rowClass = cx('columns', {
-    [styles.isCollapsed]: isCollapsed
-  });
+  const rowClass = isCollapsed =>
+    cx('columns', {
+      [styles.isCollapsed]: isCollapsed
+    });
 
   return (
-    <div className='columns'>
-      <div className='column'>
-        <p>Authorised: {isAuthorised.toString()}</p>
-        <button className='button is-dark is-large' onClick={toggle}>
-          Hellow
-        </button>
-        <button className='button is-large is-warning' onClick={toggleAuth}>
-          {isAuthorised ? 'logout' : 'login'}
-        </button>
-        {isAuthorised ? (
-          <div className={rowClass}>
-            <div className='column'>
-              <div className='notification is-primary'>First column</div>
+    <Collapse
+      render={({ toggle, isCollapsed }) => (
+        <Authorisation
+          render={({ toggleAuth, isAuthorised }) => (
+            <div className='columns'>
+              <div className='column'>
+                <p>Authorised: {isAuthorised.toString()}</p>
+                <button className='button is-dark is-large' onClick={toggle}>
+                  Hellow
+                </button>
+                <button
+                  className='button is-large is-warning'
+                  onClick={toggleAuth}
+                >
+                  {isAuthorised ? 'logout' : 'login'}
+                </button>
+                {isAuthorised ? (
+                  <div className={rowClass(isCollapsed)}>
+                    <div className='column'>
+                      <div className='notification is-primary'>
+                        First column
+                      </div>
+                    </div>
+                    <div className='column'>
+                      <div className='notification is-primary'>
+                        Second column
+                      </div>
+                    </div>
+                    <div className='column'>
+                      <div className='notification is-primary'>
+                        Third column
+                      </div>
+                    </div>
+                    <div className='column'>
+                      <div className='notification is-primary'>
+                        Fourth column
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <h2 className='title '>You must sign in</h2>
+                )}
+              </div>
             </div>
-            <div className='column'>
-              <div className='notification is-primary'>Second column</div>
-            </div>
-            <div className='column'>
-              <div className='notification is-primary'>Third column</div>
-            </div>
-            <div className='column'>
-              <div className='notification is-primary'>Fourth column</div>
-            </div>
-          </div>
-        ) : (
-          <h2 className='title '>You must sign in</h2>
-        )}
-      </div>
-    </div>
+          )}
+        />
+      )}
+    />
   );
 };
 
